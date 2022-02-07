b1c9b8445a3a3a1669234a24c1c88ed6ec27ebdc
JohandroG <johandrogonzalez@gmail.com>
2022-02-07 14:15:16 -0600

    Fortgot 2

--- public/src/app/forgot-pass2/forgot-pass2.component.ts
+++ public/src/app/forgot-pass2/forgot-pass2.component.ts
@@ -1,7 +1,15 @@
-import { Component, OnInit } from '@angular/core';
 import {Router, ActivatedRoute, Params} from '@angular/router';
 import {UsersService} from '../services/users.service';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
 
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-forgot-pass2',
@@ -24,6 +32,14 @@ msj:any = {};
 
 //!--VARIABLES------------------------------------------------------------------------------------------
 
+codeFormControl = new FormControl('', [Validators.required]);
+
+passFormControl = new FormControl('', [Validators.required,Validators.minLength(8),Validators.pattern(/^\S*$/)]);
+
+confpassFormControl = new FormControl('', [Validators.required,Validators.minLength(8),Validators.pattern(/^\S*$/)]);
+
+matcher = new MyErrorStateMatcher();
+
   constructor(private _HttpService: UsersService,
     private _router:Router,
     private _route:ActivatedRoute,
@@ -33,7 +49,8 @@ msj:any = {};
   }
 
   request_recover_pass(event:any):void{
-    this._route.params.subscribe((params:any) => this.requestinfo._id = params.id)
+    if(!this.passFormControl.errors && !this.codeFormControl.errors && !this.confpassFormControl.errors){
+      this._route.params.subscribe((params:any) => this.requestinfo._id = params.id)
     console.log(this.requestinfo._id);
     
     this.msj = {}
@@ -43,6 +60,10 @@ msj:any = {};
     },(error=>{
       this.msj = error.error
     }))
+    }
+    else{
+      this.msj.problem = "⚠️ Ha ocurrido un problema"
+    }
   }
 
 }

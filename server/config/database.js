02fcfc9c9fc07e2daedea03738a51017a83e8179
JohandroG <johandrogonzalez@gmail.com>
2022-02-11 15:51:40 -0600

    Images Working

--- server/config/database.js
+++ server/config/database.js
@@ -6,4 +6,4 @@ var mongoose = require("mongoose");
 
 
 //!FOR PROD PORPOSES
-mongoose.connect("mongodb+srv://johang:201010jk@cluster0.anbzh.mongodb.net/NoticeBoard?retryWrites=true&w=majority");
+mongoose.connect(process.env.MONGOATLASLOG);

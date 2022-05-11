1981e4d890359258f5b61a46416decec57330bbf
JohandroG <johandrogonzalez@gmail.com>
2022-05-10 21:58:42 -0600

    make it better

--- server.js
+++ server.js
@@ -19,12 +19,12 @@ app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 app.use("/resources", express.static(path.join(__dirname + "/resources")));
 
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, PUT, OPTION");
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, PUT, OPTION");
+//     next();
+// });
 
 //*DATABASE------------------------------------------------------------------------------------------
 require("./server/config/database.js");
